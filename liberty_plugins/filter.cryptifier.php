e0ea2d2bad4287ea4b06c9296eeec12554850af0
Christian Fowler <spider@viovio.com>
2009-05-01 14:06:52 +0000

    check for pObject existence

--- liberty_plugins/filter.cryptifier.php
+++ liberty_plugins/filter.cryptifier.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version  $Header: /cvsroot/bitweaver/_bit_cryptifier/liberty_plugins/filter.cryptifier.php,v 1.5 2009/03/31 15:39:24 spiderr Exp $
+ * @version  $Header: /cvsroot/bitweaver/_bit_cryptifier/liberty_plugins/filter.cryptifier.php,v 1.6 2009/05/01 14:06:52 spiderr Exp $
  * @package  liberty
  * @subpackage plugins_filter
  */
@@ -54,7 +54,7 @@ function cryptifier_prefilter( &$pData, &$pFilterHash, $pObject ) {
 	global $gBitSystem, $gBitThemes, $gBitSmarty;
 	// Decrypt the content if needed and able
 
-	if( $pObject->getPreference( 'cryptifier_cipher' ) && $pObject->getPreference( 'cryptifier_scope' ) == 'all' ) {
+	if( $pObject && $pObject->getPreference( 'cryptifier_cipher' ) && $pObject->getPreference( 'cryptifier_scope' ) == 'all' ) {
 		$pObject->verifyUserPermission( 'p_cryptifier_decrypt_content' );
 		if( !empty( $_REQUEST['cryptifier_cipher_key'] ) ) {
 			$pData = cryptifier_decrypt_data( $pData, $pObject->getPreference( 'cryptifier_cipher' ), $_REQUEST['cryptifier_cipher_key'], $pObject->getPreference( 'cryptifier_iv' ) );			
